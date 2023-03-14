eaf739540c38e73426061ddf74a81c057e3f823e
Mircea <plmn170999@gmail.com>
2023-03-14 11:57:09 +0100

    aggiunte

--- API-Progetto/serverless.ts
+++ API-Progetto/serverless.ts
@@ -1,12 +1,13 @@
 import type { AWS } from '@serverless/typescript';
-
+import { environment } from 'src/environement/environement';
 import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
   service: 'api-progetto',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline',/*for dynamodblocal*/'serverless-dynamodb-local'],
   provider: {
+    region: environment.awsRegion,
     name: 'aws',
     runtime: 'nodejs14.x',
     apiGateway: {
@@ -16,6 +17,29 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  resources: {
+    Resources: {
+      userTable: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: environment.dynamo.userData.tableName,
+          BillingMode: 'PAY_PER_REQUEST',
+          AttributeDefinitions: [
+            {
+              AttributeName: 'username',
+              AttributeType: 'S',
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: 'username',
+              KeyType: 'HASH',
+            },
+          ],
+        },
+      },
     },
   },
   // import the function via paths
@@ -32,7 +56,16 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    //extra per testare dynamodb in locale
+    dynamodb: {
+      stages: 'dev',
+      start: {
+        port: 8000,
+        inmemory: true,
+        migrate: true,
+      }
+    }
   },
 };
 
-module.exports = serverlessConfiguration;
+module.exports = serverlessConfiguration; 
